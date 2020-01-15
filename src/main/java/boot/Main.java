fb654bc5eb4aa68e4da65706e43c817f49b352ea
Artemev Roman <rom1000rom@yandex.ru>
2020-01-15 09:53:39 +0300

    Initial commit

---
+++ src/main/java/boot/Main.java
@@ -0,0 +1,13 @@
+package boot;
+
+import java.util.logging.Logger;
+
+public class Main
+{
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args)
+    {
+        log.info("Hello world!");
+    }
+}
