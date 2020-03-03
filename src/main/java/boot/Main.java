22a442c042f8ec8381b24166b4fadeb1f8a689ce
Artemev Roman <rom1000rom@yandex.ru>
2020-03-03 13:49:35 +0300

    Add common operation.

--- src/main/java/boot/Main.java
+++ src/main/java/boot/Main.java
@@ -1,6 +1,10 @@
 package boot;
 
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class Main
 {
@@ -9,5 +13,15 @@ public class Main
     public static void main(String[] args)
     {
         log.info("Hello world!");
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        log.info(list.toString() + list.get(0).getClass());
+
+        List<String> listString = list.stream().filter((n) -> (n % 2) != 0).map((n) ->
+                n.toString()).collect(Collectors.toList());
+
+        log.info(listString.toString()  + listString.get(0).getClass());
+
     }
+
 }
